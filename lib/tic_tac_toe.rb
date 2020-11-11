933d78231b6fe53899c3bbee40ddab902efba7cd
Thor Summerall <thor.summerall@gmail.com>
2020-11-11 15:29:20 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -23,17 +23,16 @@ def valid_move?(board, index)
 end
 
 def turn(board)
-    puts "Please choose a number 1-9:"
-    user_input = gets.chomp
-    index = input_to_index(user_input)
-    if valid_move?(board, index)
-      player_token = current_player(board)
-      move(board, index, player_token)
-      display_board(board)
-    else
-      turn(board)
-    end
-  end  
+  puts "Please enter 1-9:"
+  input = gets.strip
+  index = input_to_index(input)
+  if valid_move?(board, index)
+    move(board, index)
+    display_board(board)
+  else
+    turn(board)
+  end
+end
 
  def play(board)
     until over?(board) == true 
